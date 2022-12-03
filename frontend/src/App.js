53d418fc288d38ab988d9c2ee090f6e209d158c7
Kiran-naregal <01fe20bcs193@kletech.ac.in>
2022-12-03 13:21:33 +0530

    inital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+// import './App.css';
+// import logo from './logo.svg';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Navbar from "./components/navbar.components";
+import Home from "./components/home.components";  
+import Approve from './components/approval.components';
+import Display from './components/printApproved.component';
+
+function App() {
+
+  return (
+    <Router>
+      <div className='container'>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element = {<Home/>} />
+          <Route path='/update' exact element={<Approve />}/>
+          <Route path='/Print' exact element={<Display />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
